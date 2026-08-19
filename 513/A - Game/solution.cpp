09a72070ede7d8dde2785fc281be0a46418fce34
OnikawaYozy <topaznguyen99@gmail.com>
2026-08-19 16:00:05 +0700

    Add Game [A] from Codeforces

---
+++ 513/A - Game/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cin.tie(0); 
+    ll n1, n2, k1, k2;
+    cin >> n1 >> n2 >> k1 >> k2;
+    if (n1 > n2) cout << "First
+";
+    else cout << "Second
+";
+    return 0;
+}
